bad79c0df73346723bb30f286e1a6f903203aff1
rohangawade-2604 <rohangawade2604@gmail.com>
2026-03-12 18:40:35 +0530

    cide

--- Rohan_Backend/src/Routes/nestedhierarchyroutes.js
+++ Rohan_Backend/src/Routes/nestedhierarchyroutes.js
@@ -1,32 +1,73 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const {Tlm} = require("../../Folders/UserModels/TLM")
+const { Tlm, Slm, Flm, Mr } = require("../../Folders/UserModels/TLM");
 
-router.get("/hierarchy", async (req, res) => {
+router.get("/hierarchy/:id", async (req, res) => {
   try {
+    const { id } = req.params;
+    let data = null;
+    let role = "";
 
-    const data = await Tlm.find()
-      .populate({
-        path: "slm",
+    data = await Tlm.findById(id).populate({
+      path: "slm",
+      populate: {
+        path: "flm",
         populate: {
-          path: "flm",
-          populate: {
-            path: "Mr"
-          }
-        }
-      });
+          path: "Mr",
+        },
+      },
+    });
+
+    // main logic concept work for params
+
+    //--------- if TLM object id get here
+    if (data) {
+      role = "TLM";
+      return res.json({ role, data });
+    }
+    // check for the slm
+    data = await Slm.findById(id).populate({
+      path: "flm",
+      populate: {
+        path: "Mr",
+      },
+    });
+
+    //--------- if SLM object id get here
+    if (data) {
+      role = "SLM";
+      return res.json({ role, data });
+    }
+    // check for the FLM
+    data = await Flm.findById(id).populate("Mr");
+
+    //--------- if FLM object id get here
+    if (data) {
+      role = "FLM";
+      return res.json({ role, data });
+    }
+    // check for the MR
+    data = await Mr.findById(id);
+
+    //--------- if Mr object id get here
+    if (data) {
+      role = "MR";
+      return res.json({ role, data });
+    }
+
+    res.status(404).json({ message: "ID not founded" });
 
     res.json({
       success: true,
-      data
+      data,
     });
-
+    
   } catch (error) {
     res.status(500).json({
-      error: error.message
+      error: error.message,
     });
   }
 });
 
-module.exports = router
+module.exports = router;
