59b054820e2a6a5ff09b90018000acffaa01fb49
rohangawade-2604 <rohangawade2604@gmail.com>
2026-03-13 11:26:29 +0530

    code

---
+++ Rohan_Backend/src/Controller/nestedhierarchy.js
@@ -0,0 +1,70 @@
+const { Tlm, Slm, Flm, Mr } = require("../../Folders/UserModels/TLM");
+
+const nestedhierarchy = async (res, req) => {
+      try {
+    const { id } = req.params;
+    let data = null;
+    let role = "";
+
+    data = await Tlm.findById(id).populate({
+      path: "slm",
+      populate: {
+        path: "flm",
+        populate: {
+          path: "Mr",
+        },
+      },
+    });
+
+    // main logic concept work for params
+
+    //--------- if TLM object id get here
+    if (data) {
+      role = "TLM";
+      return res.json({ role, data });
+    }
+    // check for the slm
+    data = await Slm.findById(id).populate({
+      path: "flm",
+      populate: {
+        path: "Mr",
+      },
+    });
+
+    //--------- if SLM object id get here
+    if (data) {
+      role = "SLM";
+      return res.json({ role, data });
+    }
+    // check for the FLM
+    data = await Flm.findById(id).populate("Mr");
+
+    //--------- if FLM object id get here
+    if (data) {
+      role = "FLM";
+      return res.json({ role, data });
+    }
+    // check for the MR
+    data = await Mr.findById(id);
+
+    //--------- if Mr object id get here
+    if (data) {
+      role = "MR";
+      return res.json({ role, data });
+    }
+
+    res.status(404).json({ message: "ID not founded" });
+
+    res.json({
+      success: true,
+      data,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+}
+
+module.exports = {nestedhierarchy}
--- Rohan_Backend/src/Routes/nestedhierarchyroutes.js
+++ Rohan_Backend/src/Routes/nestedhierarchyroutes.js
@@ -1,73 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { Tlm, Slm, Flm, Mr } = require("../../Folders/UserModels/TLM");
+const {nestedhierarchy} = require("../Controller/nestedhierarchy")
 
-router.get("/hierarchy/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    let data = null;
-    let role = "";
+// const { Tlm, Slm, Flm, Mr } = require("../../Folders/UserModels/TLM");
 
-    data = await Tlm.findById(id).populate({
-      path: "slm",
-      populate: {
-        path: "flm",
-        populate: {
-          path: "Mr",
-        },
-      },
-    });
 
-    // main logic concept work for params
-
-    //--------- if TLM object id get here
-    if (data) {
-      role = "TLM";
-      return res.json({ role, data });
-    }
-    // check for the slm
-    data = await Slm.findById(id).populate({
-      path: "flm",
-      populate: {
-        path: "Mr",
-      },
-    });
-
-    //--------- if SLM object id get here
-    if (data) {
-      role = "SLM";
-      return res.json({ role, data });
-    }
-    // check for the FLM
-    data = await Flm.findById(id).populate("Mr");
-
-    //--------- if FLM object id get here
-    if (data) {
-      role = "FLM";
-      return res.json({ role, data });
-    }
-    // check for the MR
-    data = await Mr.findById(id);
-
-    //--------- if Mr object id get here
-    if (data) {
-      role = "MR";
-      return res.json({ role, data });
-    }
-
-    res.status(404).json({ message: "ID not founded" });
-
-    res.json({
-      success: true,
-      data,
-    });
-
-  } catch (error) {
-    res.status(500).json({
-      error: error.message,
-    });
-  }
-});
-
+router.get("/hierarchy", nestedhierarchy)
+router.get("/hierarchy/:id", nestedhierarchy)
 module.exports = router;
